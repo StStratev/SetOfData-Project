adf9582972419df0415288afe5443d29550144c4
Georgi Kalchev <GSKalchev18@codingburgas.bg>
2020-11-03 22:57:07 +0200

    Add comments to ConsoleApplication4.cpp

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 int main()
 {
-    int array1[100], NumberOfElements1, array2[100], NumberOfElements2, intersection[100], counter = 0;
+    int array1[100], NumberOfElements1, array2[100], NumberOfElements2, intersection[100], counter = 0; // declare variables of type int
     Greetings();
-    while (Menu(array1, NumberOfElements1, array2, NumberOfElements2));
+    while (Menu(array1, NumberOfElements1, array2, NumberOfElements2)); // when the Menu function returns true, the cycle continues until a false value is returned
     Exit();
-    return 0;
+    return 0; // stops the program
 }
