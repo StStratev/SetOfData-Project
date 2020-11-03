69da75504bd4d89afec3baa2c2997e098754c37c
kyayordanov18 <kyayordanov18@codingburgas.bg>
2020-11-03 21:27:56 +0200

    Add functions for Symetric difference and Prime numbers

--- ConsoleApplication4/Function.cpp
+++ ConsoleApplication4/Function.cpp
@@ -67,3 +67,111 @@ void Union(int* array1, int& NumberOfElements1, int* array2, int& NumberOfElemen
     }
     cout << endl;
 }
+void SymetricDifference(int* array1, int& NumberOfElements1, int* array2, int& NumberOfElements2)
+{
+    int symetry[100];
+    int counter = 0;
+    int found = 0;
+    for (int i = 0; i < NumberOfElements1; i++)
+    {
+        found = 0;
+        for (int j = 0; j < NumberOfElements2; j++)
+        {
+            if (array1[i] == array2[j])
+            {
+                found++;
+            }
+        }
+        if (found == 0)
+        {
+            symetry[counter++] = array1[i];
+        }
+    }
+    for (int j = 0; j < NumberOfElements2; j++)
+    {
+        found = 0;
+        for (int i = 0; i < NumberOfElements1; i++)
+        {
+            if (array2[j] == array1[i])
+            {
+                found++;
+            }
+        }
+        if (found == 0)
+        {
+            symetry[counter++] = array2[j];
+        }
+    }
+    cout << "The Symetric diffrence between the two sets of data: ";
+    cout << counter << endl;
+    for (int i = 0; i < counter; i++)
+    {
+        cout << symetry[i] << " ";
+    }
+    sort(symetry, symetry + counter);
+    cout << endl;
+}
+void PrimeNumbers(int* array1, int& NumberOfElements1, int* array2, int& NumberOfElements2)
+{
+k:
+    int counter = 0;
+    cout << "Wich array do you want to work with?" << endl;
+    cout << "Array1 - 1 \nArray2 - 2\n";
+    cout << "Choose : ";
+    int n;
+    cin >> n;
+    cout << endl;
+    if (n == 1)
+    {
+        cout << "The prime numbers in the first set of data are: ";
+        for (int i = 0; i < NumberOfElements1; i++)
+        {
+            bool prime = true;
+            for (int j = 2; j * j <= array1[i]; j++)
+            {
+                if (array1[i] % j == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+            if (array1[i] == 1)
+            {
+                prime = false;
+            }
+            if (prime) cout << array1[i] << " ";
+        }
+        sort(array1, array1 + counter);
+        cout << endl << endl << endl;
+
+    }
+    else if (n == 2)
+    {
+        cout << "The prime numbers in the second set of data are: ";
+        for (int i = 0; i < NumberOfElements1; i++)
+        {
+            bool prime = true;
+            for (int j = 2; j * j <= array2[i]; j++)
+            {
+                if (array2[i] % j == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+            if (array2[i] == 1)
+            {
+                prime = false;
+            }
+            if (prime) cout << array2[i] << " ";
+        }
+        sort(array2, array2 + counter);
+        cout << endl << endl << endl;
+    }
+    else
+    {
+        cout << "Incorrect input! Please try again!";
+        goto k;
+    }
+    cout << "-----------------------------------------------" << endl << endl << endl;
+}
--- ConsoleApplication4/Function.h
+++ ConsoleApplication4/Function.h
@@ -1,3 +1,5 @@
 #pragma once
 void Intersection(int* array1, int& NumberOfElements1, int* array2, int& NumberOfElements2);
 void Union(int* array1, int& NumberOfElements1, int* array2, int& NumberOfElements2);
+void SymetricDifference(int* array1, int& NumberOfElements1, int* array2, int& NumberOfElements2);
+void PrimeNumbers(int* array1, int& NumberOfElements1, int* array2, int& NumberOfElements2);
